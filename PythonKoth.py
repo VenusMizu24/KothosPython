c98c5e6095670a09288e777e1710773585f63b1d
Nephtiry Bailey <vnusmizu24@yahoo.com>
2015-10-31 16:46:20 -0700

    code for creating folders and log written

---
+++ PythonKoth.py
@@ -0,0 +1,29 @@
+__author__ = 'Nephtiry'
+from selenium import webdriver
+import os
+import time
+import datetime
+import json
+
+class create:
+    def createDir(dir):
+        ts = time.time()
+        os.mkdir('C:\\temp\KothosTest_'+(datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M')))
+        global folder
+        folder=('C:\\temp\KothosTest_'+(datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M')))
+        os.mkdir(folder+'\Screenshots')
+        screenfolder=(folder+'\Screenshots')
+
+    def createLog(txt):
+        filename=open(folder+'\KothosAutoTest_'+(datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M'))+'.log', 'w+')
+
+
+
+
+
+
+
+driver = webdriver.Chrome()
+driver.get('http://www.thecityofkothos.com/')
+
+class gather:
